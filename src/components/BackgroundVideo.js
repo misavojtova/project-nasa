3937ce88933f88ee3078713dc4be8e3c17b50e5c
misa vojtova <misacastella@gmail.com>
2021-11-19 10:10:14 +0000

    deploy diogo

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -14,7 +14,7 @@ function BackgroundVideo() {
           width: "100%",
           left: "50%",
           top: "50%",
-          height: "100vh",
+          height: "100%",
           objectFit: "cover",
           transform: "translate(-50%, -50%)",
           display: "flex",
