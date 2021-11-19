a5f8ccc8928b6b2b4675d94cc378c5ef8e208086
joaotcmeirinho <jmeiradas@gmail.com>
2021-11-19 09:57:55 +0000

    fixed magnifying glass position

--- src/components/ImgSearchForm.js
+++ src/components/ImgSearchForm.js
@@ -10,26 +10,28 @@ function ImgSearchForm({
 }) {
   return (
     <form onSubmit={handleGetRequest} className="img-search-wrapper">
-      {images.length > 0 && error === null ? (
-        <div>
-          <button onClick={handleClearButton} className="clear-btn">
-            Clear
-          </button>
-        </div>
-      ) : (
-        <div></div>
-      )}
-      <input
-        className="img-search-input"
-        type="search"
-        autoComplete="off"
-        name="searchValue"
-        placeholder="Search"
-        onChange={e => setSearchTerm(e.target.value)}
-      />
-      <button type="submit" className="img-search-btn">
-        {<BsSearch />}
-      </button>
+      <div className="clear-search-glass">
+        {images.length > 0 && error === null ? (
+          <div>
+            <button onClick={handleClearButton} className="clear-btn">
+              Clear
+            </button>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        <input
+          className="img-search-input"
+          type="search"
+          autoComplete="off"
+          name="searchValue"
+          placeholder="Search"
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <button type="submit" className="img-search-btn">
+          {<BsSearch />}
+        </button>
+      </div>
     </form>
   );
 }
